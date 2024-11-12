c714e4d8d7150e88e421b591fef8b04fa2e361cc
Saar <saarbu@post.bgu.ac.il>
2024-11-12 16:38:32 +0200

    calc_gen_ai_features updated

--- calc_gen_ai_features.py
+++ calc_gen_ai_features.py
@@ -5,19 +5,13 @@ from transformers import pipeline
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, BitsAndBytesConfig
 import time
 import torch
-
+import os
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'Using device: {device}')
 
-# Load SBERT model
-#model = SentenceTransformer('all-mpnet-base-v2')
 # Load the zero-shot classification pipeline
 classifier = pipeline("zero-shot-classification", model="MoritzLaurer/deberta-v3-large-zeroshot-v2.0")
-
-# Local bert
-#nli_model = AutoModelForSequenceClassification.from_pretrained('facebook/bart-large-mnli')
-#nli_model = AutoModelForSequenceClassification.from_pretrained("MoritzLaurer/deberta-v3-large-zeroshot-v2.0")
 
 bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
@@ -25,43 +19,41 @@ bnb_config = BitsAndBytesConfig(
     bnb_4bit_compute_dtype=torch.float16,
 )
 
-#torch.backends.cuda.matmul.allow_tf32 = True
-
-
+#
 nli_model = AutoModelForSequenceClassification.from_pretrained(
     "MoritzLaurer/deberta-v3-large-zeroshot-v2.0",
-    #quantization_config=bnb_config,
+    # quantization_config=bnb_config,
     # torch_dtype=torch.float16,
 )
 
 nli_model.to(device)
 
-#tokenizer = AutoTokenizer.from_pretrained('facebook/bart-large-mnli')
 tokenizer = AutoTokenizer.from_pretrained("MoritzLaurer/deberta-v3-large-zeroshot-v2.0")
 
+# Old experiment - Load SBERT model
+#model = SentenceTransformer('all-mpnet-base-v2')
+
+# Old experiment using sentence-bert.
 def encode_text(text):
     """Encode text using the SBERT model."""
     return model.encode(text, convert_to_tensor=True)
 
-
+# Old cosine similarity experiment - not used
 def compute_cosine_similarity(text_embeddings, concept_embeddings):
     """Compute cosine similarity between text and concept embeddings."""
     return util.pytorch_cos_sim(text_embeddings, concept_embeddings)
 
-
+# Old cosine similarity experiment - not used
 def compute_best_scores(cosine_scores, ai_features):
     """Compute the best scores for each concept word for each text."""
     best_scores = []
     for scores in cosine_scores:
-        # print("scores")
-        # print(scores)
         best_score = {ai_features[i]: scores[i].item() for i in range(len(ai_features))}
-        # print("best_score")
-        # print(best_score)
         best_scores.append(best_score)
     return best_scores
 
 
+# Old cosine similarity experiment - not used
 def process_file(file_index, ai_features):
     """Process a single file, compute scores, and concatenate results."""
     file_name = f'clusters csv/{file_index}_data.csv'
@@ -84,9 +76,10 @@ def process_file(file_index, ai_features):
     result_df = pd.concat([data, scores_df], axis=1)
 
     # Save the result back to the original CSV file
-    #result_df.to_csv(file_name, index=False)
+    # result_df.to_csv(file_name, index=False)
 
 
+# Cosine similarity experiment - not used
 def compute_dimension_wise_cosine_similarity(text_embedding, concept_embeddings):
     """Compute the best dimension-wise cosine similarity for a text embedding and each concept embedding."""
     best_similarities = []
@@ -107,6 +100,7 @@ def compute_dimension_wise_cosine_similarity(text_embedding, concept_embeddings)
     return best_similarities
 
 
+# Cosine similarity experiment - not used
 def compute_best_scores_dimension_wise(text_embeddings, concept_embeddings, ai_features):
     """Compute the best scores for each concept word for each text using dimension-wise cosine similarity."""
     best_scores = []
@@ -117,6 +111,7 @@ def compute_best_scores_dimension_wise(text_embeddings, concept_embeddings, ai_f
     return best_scores
 
 
+# Cosine similarity experiment - not used
 def process_file_dimension_wise(file_index, ai_features):
     """Process a single file, compute scores using dimension-wise cosine similarity, and concatenate results."""
     file_name = f'clusters csv/{file_index}_data.csv'
@@ -140,6 +135,7 @@ def process_file_dimension_wise(file_index, ai_features):
     result_df.to_csv('bert_test.csv', index=False)
 
 
+# Legacy code
 def classify_sentence(sentence, ai_features):
     """
     Classifies the sentence against a list of concepts using zero-shot classification.
@@ -158,40 +154,36 @@ def classify_sentence(sentence, ai_features):
     return concept_scores
 
 
-def process_file_with_classification(file_index, ai_features):
+def process_file_with_classification(file_index, ai_features, data_files_location):
     """Process a single file, compute scores, and concatenate results."""
-    file_name = f'clusters csv/{file_index}_data.csv'
+    file_name = os.path.join(data_files_location, f'{file_index}_data.csv')
     data = pd.read_csv(file_name)
     cluster_text = data['text']
 
-    # Compute the best scores for each text against the concept words
-    # scores_list = [classify_sentence(text, ai_features) for text in cluster_text]
-
     scores_list = []
     for count, text in enumerate(cluster_text):
-        print(f'Processing {count+1}/{len(cluster_text)} of cluster {file_index}')
+        print(f'Processing {count + 1}/{len(cluster_text)} of cluster {file_index}')
         start = time.time()
         probabilities = compute_probabilities(text, ai_features)
-        # print(f'probabilities: {probabilities}')
         scores_list.append(probabilities)
         end = time.time()
         elapsed = end - start
         print(f'elapsed time: {elapsed}')
 
-    #print(f'score 1: {scores_list[0]}')
     # Convert the list of scores to a DataFrame
     scores_df = pd.DataFrame(scores_list)
 
     # Combine the scores with the original dataframe
     result_df = pd.concat([data, scores_df], axis=1)
-    #print(f'results: {result_df.head}')
     # Save the result back to the original CSV file
     result_df.to_csv(file_name, index=False)
+    print(f'Finished processing cluster {file_index}')
 
 
 def compute_probabilities(sentence, hypotheses):
     """
     Computes probabilities for a sentence and a list of hypotheses.
+    In this case - relevance scores of the hypotheses texts to the sentence.
 
     Args:
     sentence (str): The premise sentence.
@@ -202,60 +194,62 @@ def compute_probabilities(sentence, hypotheses):
     """
     probabilities = {}
     for hypothesis in hypotheses:
-        inputs = tokenizer.encode(sentence, hypothesis, return_tensors='pt', truncation_strategy='only_first').to(
-            device)
+        inputs = (tokenizer.encode(sentence, hypothesis, return_tensors='pt', truncation_strategy='only_first')
+                  .to(device))
 
         logits = nli_model(inputs)[0]
-
-        #entail_contradiction_logits = logits[:, [0, 2]]
         entail_contradiction_logits = logits[:, [0, 1]]
-
         probs = entail_contradiction_logits.softmax(dim=1)
-
         prob_hypothesis_true = probs[:, 1].item()
-
         probabilities[hypothesis] = prob_hypothesis_true
 
     return probabilities
 
-def deberta_for_ai_features(ai_features_file_location: str, data_files_location: str) -> None:
+
+def _check_ai_features_file(ai_features_file_location: str):
+    try:
+        clustered_ai_features = pd.read_csv(ai_features_file_location, encoding='ISO-8859-1')
+        # Validate that all column headers are integers
+        cols = clustered_ai_features.columns.tolist()
+        for col in cols:
+            int(col)  # Will raise ValueError if conversion fails
+
+        return clustered_ai_features, cols
+
+    except FileNotFoundError:
+        raise FileNotFoundError("Error: File not found. Please check the file path.")
+    except pd.errors.EmptyDataError:
+        raise pd.errors.EmptyDataError("Error: The file is empty.")
+    except pd.errors.ParserError:
+        raise pd.errors.ParserError("Error: There was an issue parsing the file. Please check the file format.")
+    except ValueError:
+        raise ValueError("Error: One of the column names is not an integer representing cluster number.")
+    except AttributeError:
+        raise AttributeError("Error: Problem with the DataFrame structure.")
+    except Exception as e:
+        raise Exception(f"An unexpected error occurred: {e}")
+
+
+def deberta_for_llm_features(ai_features_file_location: str, data_files_location: str) -> None:
     print('Torch info for running DeBERTa. Run on GPU')
     print(torch.cuda.is_available())
     print(torch.__version__)
     print(torch.cuda.get_device_name(torch.cuda.current_device()))
 
-    # Can do this more efficiently
     try:
-        clustered_ai_features, cols = check_ai_features_file(ai_features_file_location)
-
+        clustered_ai_features, cols = _check_ai_features_file(ai_features_file_location)
+        # Iterating through the clusters
+        for col in cols:
+            try:
+                ai_features = clustered_ai_features[col].dropna().tolist()
+                process_file_with_classification(col, ai_features, data_files_location)
+                print(f"Processing complete for file {col}_data.csv")
+            except ValueError:
+                # Skip non-numeric columns if they exist
+                print(f"Skipping non-numeric column: {col}")
     except Exception as e:
         print(e)
 
-    clustered_ai_features = pd.read_csv(ai_features_file_location, encoding='ISO-8859-1')
-    cols = clustered_ai_features.columns.tolist()
-
-
-def check_ai_features_file(ai_features_file_location: str):
-    try:
-        clustered_ai_features = pd.read_csv(ai_features_file_location, encoding='ISO-8859-1')
-        cols = clustered_ai_features.columns.tolist()
-        for col in cols:
-            cluster_index = int(col)
-        return clustered_ai_features, cols
-    except FileNotFoundError:
-        print("Error: File not found. Please check the file path.")
-    except pd.errors.EmptyDataError:
-        print("Error: The file is empty.")
-    except pd.errors.ParserError:
-        print("Error: There was an issue parsing the file. Please check the file format.")
-    except UnicodeDecodeError:
-        print("Error: The file encoding may be incorrect. Try using a different encoding.")
-    except ValueError:
-        print("Error: One of the column names is not an integer and cannot be converted.")
-    except AttributeError:
-        print("Error: Problem with the DataFrame structure.")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
 
 if __name__ == '__main__':
     print('Torch info for running DeBERTa. Run on GPU')
@@ -273,7 +267,7 @@ if __name__ == '__main__':
 
             # process_file(cluster_index, ai_features)
             # process_file_dimension_wise(cluster_index, ai_features)
-            process_file_with_classification(cluster_index, ai_features)
+            process_file_with_classification(cluster_index, ai_features, 'clusters csv')
 
             print(f"Processing complete for file {cluster_index}_data.csv")
 
